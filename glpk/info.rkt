3ac7f8d76f6d95192b165990b916b6698d906cfc
John Clements <clements@racket-lang.org>
2017-06-20 15:09:45 -0700

    basic setup files

---
+++ glpk/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define name "Gnu Linear Programming Kit")
+
+;(define compile-omit-paths '("experimental" "paper" "soups-paper" "user-study-2016"))
+;(define test-omit-paths '("experimental" "paper" "soups-paper" "user-study-2016"))
+;(define scribblings '(("molis-hai.scrbl" () (tool))))
